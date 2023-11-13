29eeb5b3127ecb9ae76ac9bc755c185ae828624f
Jonas Gao <gaopinsong@live.cn>
2023-11-13 15:37:50 +0800

    feat: Pass file path from args.

---
+++ README.md
@@ -0,0 +1,4 @@
+tomcat-porter
+=============
+
+A little tool to read tomcat server.xml and print all using port.
--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
-	server, err := parse("example.xml")
+	if len(os.Args) < 2 {
+		_, _ = fmt.Fprintln(os.Stderr, "Error: there is no input file path.")
+		os.Exit(1)
+		return
+	}
+	server, err := parse(os.Args[1])
 	if err != nil {
 		fmt.Println("Failed parse xml file.")
 		fmt.Println(err)
-		os.Exit(1)
+		os.Exit(2)
 		return
 	}
 	fmt.Print("Using port has, server = " + server.Port)
